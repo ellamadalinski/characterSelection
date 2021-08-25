15c7eac57fdf0bac30e6baca46059309fa8b0715
Ella Madalinski <ella.madalinski24@d155.org>
2021-08-25 08:08:43 -0500

    Character selection and accessory selection

---
+++ UIImageView_CharacterSelection/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  UIImageView_CharacterSelection
+//
+//  Created by ELLA MADALINSKI on 8/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var characterImageOutlet: UIImageView!
+    
+    @IBOutlet weak var accessoryImageOutlet: UIImageView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+    @IBAction func shrekButton(_ sender: UIButton) {
+        characterImageOutlet.image = UIImage(named: "shrek")
+    }
+    
+    @IBAction func donkeyButton(_ sender: UIButton) {
+        characterImageOutlet.image = UIImage(named: "donkey")
+    }
+    
+    
+    @IBAction func fionaButton(_ sender: UIButton) {
+        characterImageOutlet.image = UIImage(named: "fiona")
+    }
+    
+    
+    @IBAction func shrekAccessory(_ sender: UIButton) {
+        accessoryImageOutlet.image = UIImage(named: "lightsaber")
+    }
+    
+    @IBAction func donkeyAccessory(_ sender: UIButton) {
+        accessoryImageOutlet.image = UIImage(named: "waffle")
+    }
+    
+    @IBAction func fionaAccessory(_ sender: UIButton) {
+        accessoryImageOutlet.image = UIImage(named: "purse")
+    }
+    
+    
+}
+
